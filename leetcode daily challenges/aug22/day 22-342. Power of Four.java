2186a952526117aecfc2cac2cb77fbd96d6a93f5
ASEEMA PARVEEN AZ <96627332+aseemaparveenaz@users.noreply.github.com>
2022-08-22 21:46:05 +0530

    Create day 22-342. Power of Four.java

---
+++ leetcode daily challenges/aug22/day 22-342. Power of Four.java
@@ -0,0 +1,35 @@
+/* Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+ 
+
+Example 1:
+
+Input: n = 16
+Output: true
+Example 2:
+
+Input: n = 5
+Output: false
+Example 3:
+
+Input: n = 1
+Output: true */
+
+class Solution {
+    /* 1) (num&(num-1))==0  to check the number is 2 power k ex:16 2 power 4
+    2&2-1 is 0 while we do in bits
+     2) 1,4, 16 are %3==1 so both condition should satisfy*/
+    public boolean isPowerOfFour(int num) {
+     if(num>0)
+     {
+        if((num&(num-1))==0 && (num%3==1))
+            return true;
+         else
+             return false;
+     }
+    else
+        return false;
+    }
+}
