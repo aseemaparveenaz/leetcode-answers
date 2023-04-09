607e6f1fd8d0f9564ddbe1e8aa4f28f6d1137ca3
ASEEMA PARVEEN AZ <96627332+aseemaparveenaz@users.noreply.github.com>
2023-04-09 19:54:05 +0530

    Create day8_133.clone graph.java

---
+++ leetcode daily challenges/april23/day8_133.clone graph.java
@@ -0,0 +1,90 @@
+/*
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+
+Test case format:
+
+For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+
+An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+
+ 
+
+Example 1:
+
+
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+Example 2:
+
+
+Input: adjList = [[]]
+Output: [[]]
+Explanation: Note that the input contains one empty list. The graph consists of only one node with val = 1 and it does not have any neighbors.
+Example 3:
+
+Input: adjList = []
+Output: []
+Explanation: This an empty graph, it does not have any nodes.
+*/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+     // undirectional means 1 is the neighbour of 2-> and 2 is the neighbourof 1 <-
+     if(node==null) return null;
+    // take node as integer and neighbours as nodes in map
+     Map<Integer,Node> m=new HashMap<>();
+     return copyClonegraph(node,m); //dfs we are visiting every single node single time so T=O(N) since we create a copy S=O(N)
+    }
+
+    public Node copyClonegraph(Node node, Map<Integer,Node> cmap)
+    {
+        if(cmap.containsKey(node.val)) return cmap.get(node.val); //returns
+
+        Node copy=new Node(node.val);
+        cmap.put(node.val,copy);
+
+        for(Node neighbor:node.neighbors)
+        {
+            copy.neighbors.add(copyClonegraph(neighbor,cmap));
+        }
+
+       return copy;
+    }
+}
